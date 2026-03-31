4e59004df9ecd40800565395517854eb9ecac845
Pratyush Kumar <pratyushkumarindia8@gmail.com>
2026-03-31 16:44:02 +0530

    Update JWT cookies for Cross-Origin Vercel support

--- Downloads/chat-app/backend/utils/generateToken.js
+++ Downloads/chat-app/backend/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
         httpOnly: true, // prevent XSS attacks
-        sameSite: "strict", // prevent CSRF attacks
+        sameSite: process.env.NODE_ENV === "development" ? "strict" : "none", // Must be "none" for cross-origin Vercel to Render cookies
         secure: process.env.NODE_ENV !== "development",
     });
 };
